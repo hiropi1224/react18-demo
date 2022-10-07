b7b175bc3b34d0fb8ded008ea1fd969d3e61c633
Shirahamah <shirahama.hiroki@runners.co.jp>
2022-10-07 22:24:23 +0900

    💄 /index修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,12 @@
-import { Center, Text, VStack } from '@chakra-ui/react';
+import { Text } from '@chakra-ui/react';
 import type { NextPage } from 'next';
-import { Transition } from '../components/Transition';
+import { Layout } from '../components/Layout';
 
 const Home: NextPage = () => {
   return (
-    <Center>
-      <VStack border="1px" borderColor="gray.200" borderRadius="lg" p="4">
-        <Text>Transition</Text>
-        <Transition />
-      </VStack>
-    </Center>
+    <Layout>
+      <Text>React 18 Sample</Text>
+    </Layout>
   );
 };
 
