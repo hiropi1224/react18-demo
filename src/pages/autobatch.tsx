bb7270744c389a68493cb74fd2cd24d8ceea63cb
Shirahamah <shirahama.hiroki@runners.co.jp>
2022-10-07 22:24:47 +0900

    💄 pages/autobatchを追加

---
+++ src/pages/autobatch.tsx
@@ -0,0 +1,13 @@
+import { Text } from '@chakra-ui/react';
+import { NextPage } from 'next';
+import { Layout } from '../components/Layout';
+
+const AutoBatch: NextPage = () => {
+  return (
+    <Layout>
+      <Text>AutoBatch</Text>
+    </Layout>
+  );
+};
+
+export default AutoBatch;
